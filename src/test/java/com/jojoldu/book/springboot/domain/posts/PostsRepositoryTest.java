abd645c9b87fd03cd288c201c61c1c22da90ec1b
DG <thsdimaker@gmail.com>
2021-01-31 13:07:39 +0900

    20210131(ing)

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,52 @@
+//@Date : 2021-01-31
+//@Author : Dong J
+// 문제 : gradle.bundle 버전으로 인한 호환성 문제 및 import 수정
+
+package com.jojoldu.book.springboot.domain.posts;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("thsdimaker@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
+
--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,16 +1,21 @@
 package com.jojoldu.book.springboot.web.dto;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class HelloResponseDtoTest {
 
-    public void 롬복_기능_테스트(){
-
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
         String name = "test";
         int amount = 1000;
 
+        //when
         HelloResponseDto dto = new HelloResponseDto(name, amount);
 
+        //then
         assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amount);
     }
