b893786dd1cad393cd205afb9ff756cb8a6ada88
DG <thsdimaker@gmail.com>
2021-02-12 14:16:56 +0900

    20210212

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -24,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
         }
 )
+
 public class HelloControllerTest {
 
     @Autowired
